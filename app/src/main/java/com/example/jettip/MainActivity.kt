1db1ae3debd9e1c5c6d03d302419287e031ffb44
Vikalp2003 <vikalpshinde25@gmail.com>
2023-11-02 23:43:17 +0530

    change in SDK and added topheader

--- app/src/main/java/com/example/jettip/MainActivity.kt
+++ app/src/main/java/com/example/jettip/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.jettip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MovableContent
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +27,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetTipTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                MyApp {
+                    Text(text = "Hello Again")
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopHeader(){
+    Surface(modifier = Modifier.fillMaxWidth()
+        .height((150.dp))
+        .clip(CircleShape.copy(all = CornerSize(12.dp)))
+//        .clip(RoundedCornerShape(corner = CornerSize(12.dp)))
+    ) {
+        Column {
+
+        }
+    }
 }
+
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    Surface(color = MaterialTheme.colorScheme.background) {
+
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetTipTheme {
-        Greeting("Android")
+
     }
 }
