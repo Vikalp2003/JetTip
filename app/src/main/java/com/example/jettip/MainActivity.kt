a4c468ac566bc2e4ed4d0991485572710f483fe9
Vikalp2003 <vikalpshinde25@gmail.com>
2023-11-03 23:17:54 +0530

    added InputField and added some dependencies

--- app/src/main/java/com/example/jettip/MainActivity.kt
+++ app/src/main/java/com/example/jettip/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ComponentActivity() {
             JetTipTheme {
                 MyApp {
 //                    TopHeader()
-                    MainContent()
+//                    MainContent()
                 }
             }
         }
